8fecca19f0603dbe31e383e7d6af0e1c9d22f09f
adityakmr7 <adityakmr9672@gmail.com>
2024-07-24 14:49:50 +0530

    feat: refactor code

--- week-18-live/apps/user-app/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import {PrismaClient} from '@repo/db/client';
-const client = new PrismaClient();
-export default function Home() {
-  return (
-   <div>
-     <h1 className={"text-2xl"}>hello world </h1>
-   </div>
-  );
-}
